7ad2c675fc5af67cff974897f3f9e18ce70ba200
Ваше Имя <akylovadashaa@gmail.com>
2025-01-21 17:03:03 +0300

    add

--- app.py
+++ app.py
@@ -2,26 +2,28 @@ import streamlit as st
 import joblib
 import pandas as pd
 
+# Загрузка модели
 model = joblib.load('best_model.pkl')
 
+# Streamlit приложение
 def main():
-    st.title('Heart Disease Prediction')
+    st.title('Предсказание сердечных заболеваний')
 
     # Ввод данных пользователем
-    st.sidebar.header('User Input Parameters')
+    st.sidebar.header('Параметры ввода пользователя')
 
     def user_input_features():
-        age = st.sidebar.slider('Age', 0, 120, 50)
-        sex = st.sidebar.selectbox('Sex', ['M', 'F'])
-        chest_pain_type = st.sidebar.selectbox('Chest Pain Type', ['ATA', 'NAP', 'ASY', 'TA'])
-        resting_bp = st.sidebar.slider('Resting Blood Pressure', 0, 200, 120)
-        cholesterol = st.sidebar.slider('Cholesterol', 0, 600, 200)
-        fasting_bs = st.sidebar.selectbox('Fasting Blood Sugar', [0, 1])
-        resting_ecg = st.sidebar.selectbox('Resting ECG', ['Normal', 'ST', 'LVH'])
-        max_hr = st.sidebar.slider('Max Heart Rate', 0, 200, 150)
-        exercise_angina = st.sidebar.selectbox('Exercise Angina', ['N', 'Y'])
+        age = st.sidebar.slider('Возраст', 0, 120, 50)
+        sex = st.sidebar.selectbox('Пол', ['М', 'Ж'])
+        chest_pain_type = st.sidebar.selectbox('Тип боли в груди', ['ATA', 'NAP', 'ASY', 'TA'])
+        resting_bp = st.sidebar.slider('Артериальное давление в покое', 0, 200, 120)
+        cholesterol = st.sidebar.slider('Уровень холестерина', 0, 600, 200)
+        fasting_bs = st.sidebar.selectbox('Уровень сахара в крови натощак', [0, 1])
+        resting_ecg = st.sidebar.selectbox('ЭКГ в покое', ['Нормальный', 'ST', 'LVH'])
+        max_hr = st.sidebar.slider('Максимальная частота сердечных сокращений', 0, 200, 150)
+        exercise_angina = st.sidebar.selectbox('Стенокардия, вызванная физической нагрузкой', ['Нет', 'Да'])
         oldpeak = st.sidebar.slider('Oldpeak', 0.0, 10.0, 1.0)
-        st_slope = st.sidebar.selectbox('ST Slope', ['Up', 'Flat', 'Down'])
+        st_slope = st.sidebar.selectbox('Наклон ST', ['Вверх', 'Плоский', 'Вниз'])
 
         data = {
             'Age': age,
@@ -42,14 +44,14 @@ def main():
     input_df = user_input_features()
 
     # Отображение введенных данных
-    st.subheader('User Input parameters')
+    st.subheader('Параметры ввода пользователя')
     st.write(input_df)
 
     # Предсказание
-    if st.button('Predict'):
+    if st.button('Предсказать'):
         prediction = model.predict(input_df)
-        st.subheader('Prediction')
-        st.write(f'The prediction is: {"Heart Disease" if prediction[0] == 1 else "No Heart Disease"}')
+        st.subheader('Предсказание')
+        st.write(f'Предсказание: {"Сердечное заболевание" if prediction[0] == 1 else "Нет сердечного заболевания"}')
 
 if __name__ == '__main__':
     main()
